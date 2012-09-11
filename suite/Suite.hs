02825b80e7b468af83b4190c8e755c590b73e13d
Jason Reich <jason@cs.york.ac.uk>
2012-09-11 14:56:36 +0100

    Closed issue #1. Appears to have been an issue with depths. Inverted levels on `Thnk` and `Prod`.

--- suite/Suite.hs
+++ suite/Suite.hs
@@ -22,7 +22,7 @@ expect False = either (\(SomeException _) -> return ()) (const exitFailure) <=< 
 data Test = forall a. (Data a, Typeable a, Testable a) => 
             Test String a Bool Depth
 
-suite = [test1, test2, test3, test4, test5, test6, test7 {-, test8 -}]
+suite = [test1, test2, test3, test4, test5, test6, test7, test8 ]
 
 ------------------------------------------------------------------------------------
 
