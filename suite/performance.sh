0023f0599948f916c84f0e9adc78fd09aee8e39a
Jason Reich <jason@cs.york.ac.uk>
2012-09-13 18:54:06 +0100

    Adjusted script. Look for yourself.

--- suite/performance.sh
+++ suite/performance.sh
@@ -1,3 +1,4 @@
+[ $# -ne 1 ] && echo "performance.sh: Expecting one argument representing core." && exit
 [ ! -f "suite/Performance.hs" ] && echo "performance.sh: Wrong directory!" && exit
 
 mkdir -p suite/performance/Results
@@ -9,17 +10,17 @@ NAME=`date +%y%m%d-%H%M`
 cabal configure --enable-benchmarks
 cabal build
 
-echo $NAME $DESC >> "suite/performance/Results/descs.txt"
+echo $NAME $HOSTNAME $DESC >> "suite/performance/Results/descs.txt"
 DIR="suite/performance/Results/$NAME"
 mkdir $DIR
 
-for i in `seq 1 16`
+for i in `seq 1 $1`
 do
    echo
    echo
    echo
    echo "performance.sh: Benchmark -N$i:"
-   cabal bench --benchmark-options="-s 20 -u $DIR/N$i.csv +RTS -N$i"
+   cabal bench --benchmark-options="-s 20 -u $DIR/N$i.csv LSC2012 +RTS -N$i"
 done
 
 echo "performance.sh: Benchmarks complete."
